7fa790eb4294b1f8cbf8b0255531edb19c7822bf
Gulam Yelmuratov <gulamyelmuratov6@gmail.com>
2026-03-02 15:53:44 +0500

    commit

--- users/forms.py
+++ users/forms.py
@@ -1,12 +1,38 @@
 from django import forms
+from django.contrib.auth import get_user_model, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.db.models import Q
 
-from users.models import UserProfile
+User = get_user_model()
 
 
-class ProfileForm(forms.ModelForm):
+class CustomUserCreationForm(UserCreationForm):
     class Meta:
-        model = UserProfile
-        fields = '__all__'
-    def clean_message(self):
-        message = self.cleaned_data['message']
-        return message
+        model = User
+        fields = ('email', 'full_name', 'username')
+
+
+class CustomAuthenticationForm(forms.Form):
+    def __init__(self, request=None, *args, **kwargs):
+        self.request = request
+        super().__init__(*args, **kwargs)
+
+    identifier = forms.CharField(max_length=128)
+    password = forms.CharField(max_length=128)
+
+    def clean(self):
+        identifier = self.cleaned_data.get('identifier')
+        password = self.cleaned_data.get('password')
+        try:
+            user = User.objects.get(Q(username=identifier) | Q(email=identifier))
+        except User.DoesNotExist:
+            raise forms.ValidationError("User not found, please check your credentials")
+
+        credentials = {'email': user.email, 'password': password}
+
+        user_in = authenticate(self.request, **credentials)
+        if user_in is None:
+            raise forms.ValidationError("User not found, please check your credentials")
+
+        self.cleaned_data['user'] = user_in
+        return self.cleaned_data
